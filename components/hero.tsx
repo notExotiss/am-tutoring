b6b2e143fc31934e8175471101358c657ea69408
notExotiss <twoawesome.9@gmail.com>
2025-11-15 18:26:25 -0500

    ui fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
   }
 
   return (
-    <section id="hero" className="relative min-h-screen flex items-center justify-center px-6 pt-8 md:pt-0 pb-56 md:pb-8">
+    <section id="hero" className="relative min-h-screen flex items-center justify-center px-6 pt-8 md:pt-0 pb-64 md:pb-8">
 
       <div 
         className="mx-auto max-w-6xl relative z-10 w-full"
